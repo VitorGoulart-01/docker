c3074a9b12d3a9dd6d15892491a06af1cef05b67
VitorGoulart-01 <hugohmslswawa@gmail.com>
2025-11-07 00:53:40 +0000

    banco de dados

---
+++ copiar/src/main/java/exemplo/copiar/CopiarApplication.java
@@ -0,0 +1,13 @@
+package exemplo.copiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopiarApplication.class, args);
+	}
+
+}
---
+++ copiar/src/test/java/exemplo/copiar/CopiarApplicationTests.java
@@ -0,0 +1,13 @@
+package exemplo.copiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopiarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
